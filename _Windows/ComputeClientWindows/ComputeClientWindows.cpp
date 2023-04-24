e1c371e52ad9360d67f1cba06a49cd54cc3ef317
Macdaddy4sure <Macdaddy4sure@gmail.com>
2023-04-24 10:06:43 -0700

    Add files via upload

---
+++ _Windows/ComputeClientWindows/ComputeClientWindows.cpp
@@ -0,0 +1,30 @@
+/*
+    Copyright(c) 2021 Tyler Crockett | Macdaddy4sure.com
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissionsand
+    limitations under the License.
+*/
+
+#include "ComputeClientWindows.h"
+
+using namespace std;
+
+/*
+    Purpose: Write a program that resides on a computer that captures sounds and images. The images are for clean captures of the desktop where algorithms scan the images for computer use. The following is a list of required features.
+    1. Sockets programming to a local hostname; Android phone, laptop, or local server with _AI Client installed.
+
+*/
+
+int main()
+{
+    cout << "Hello World!\n";
+}
